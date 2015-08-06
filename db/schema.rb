e5bb2ed29158d35f6020bca5014ad0135a38da97
rapbore <rapbore@gmail.com>
2015-08-06 01:57:37 -0300

    set assets

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150805011726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alumnos", force: :cascade do |t|
     t.string   "nombre"
     t.integer  "curso_id"
